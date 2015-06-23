18533902544d9421a2baa74c0f05f27f7e3bbcfc
Alastair Paragas <alastairparagas@gmail.com>
2015-06-23 02:37:05 -0400

    Configuration File

---
+++ api/config.js
@@ -0,0 +1,10 @@
+(function (global) {
+	'use strict';
+	
+	var config = {
+		userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36"	
+	};
+	
+	module.exports = config;
+	
+}(global));
